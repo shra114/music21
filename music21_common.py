e0e99872e1f595ff46babefabf0fa0d65b64edee
shra114 <shravan.ashannagari@gmail.com>
2020-04-25 20:43:19 +0530

    add stream functions

--- music21_common.py
+++ music21_common.py
@@ -7,7 +7,23 @@ def show_note(mynote):
     f = note.Note(mynote)
     f.show()
 
-play_note("C")
-play_note("D")
-play_note("E")
+#play_note("C")
+#play_note("D")
+#play_note("E")
+
+def show_stream(list1):
+    stream1 = stream.Stream()
+    for i in list1:
+        stream1.append(note.Note(i))
+    stream1.show()
+
+def play_stream(list1):
+    stream1 = stream.Stream()
+    for i in list1:
+        stream1.append(note.Note(i))
+    stream1.show('midi')
+
+cmajor_scale =["C","D","E","F","G","A","B","C5"]
+play_stream(cmajor_scale)
+show_stream(cmajor_scale)
 
